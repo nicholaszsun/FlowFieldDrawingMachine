e47607e75523d9b001b95de30a18fbddd3e0bd91
nicholaszsun <nicholaszsun@gmail.com>
2022-11-15 23:22:32 -0800

    edit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,54 @@
+
+var inc = 0.01;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
+  pixelDensity(1);
+  // button = createButton('stop');
+  // button.mousePressed(pause);
+  // button =createButton('save');
+  // button.mousePressed(saved);
+
 }
 
 function draw() {
-  background(220);
+  loadPixels();
+  for (var x = 0; x < width; x++){
+    for (var y = 0; x < height; y++){
+      var index = (x + y * width) * 4;
+      var r = random(255);
+      pixels[index+0] = r;
+      pixels[index+1] = r;
+      pixels[index+2] = r;
+      pixels[index+3] = 255;
+    }
+  }
+  updatePixels();
 }
+
+// function pause(){
+//   noLoop();
+// }
+// function saved(){
+//   saveCanvas('Drawing Machine Image', 'png');
+// }
+
+// function perlnoiseinitial(){
+//   var x = map(noise(xoff1), 0, 1, 0, width);
+//   var y = map(noise(xoff2), 0, 1, 0, height);
+//   xoff1 += 0.01;
+//   xoff2 += 0.01;
+//   ellipse(x,y,30);
+// }
+
+// function perlnoisegraph(){
+//   var xoff = start;
+//   for (var x = 0; x < width; x++){
+//     stroke(255);
+//     var y = noise(xoff)*height;
+//     vertex(x,y);
+//     xoff += inc;
+//
+//   }
+// }
